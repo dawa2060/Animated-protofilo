61d16ef2cb39846649b28fc8827a2dd785eb4677
dawa2060 <85466908+dawa2060@users.noreply.github.com>
2023-04-19 00:12:16 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Student", "Beginner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true
+})
